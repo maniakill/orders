1f573b889a6895e9af3bfbfe134a17ced5eb3425
Pop Marius <maniak_is_me@yahoo.com>
2014-06-27 20:39:57 +0300

    Create lang.js

---
+++ lang/lang.js
@@ -0,0 +1,9 @@
+var LANG={};
+//dutch
+LANG[1]={'Log In':'Log In','Username':'Gebruikersnaam','Password':'Wachtwoord','Can\'t access your account':'Can\'t access your account','Please fill all the fields':'Vul alle verplichte velden in','Server error. Please try later':'Server error. Please try later'}
+//english
+LANG[2]={'Log In':'Log In','Username':'Username','Password':'Password','Can\'t access your account':'Can\'t access your account','Please fill all the fields':'Please fill all the fields','Server error. Please try later':'Server error. Please try later'}
+//french
+LANG[3]={'Log In':'Log In','Username':"Nom d'utilisateur",'Password':'Mot de passe','Can\'t access your account':'Can\'t access your account','Please fill all the fields':'Veuillez remplir tous les champs obligatoires','Server error. Please try later':'Server error. Please try later'}
+//german
+LANG[4]={'Log In':'Log In','Username':'Benutzernamen','Password':'Passwort','Can\'t access your account':'Can\'t access your account','Please fill all the fields':'Please fill all the fields','Server error. Please try later':'Server error. Please try later'}
